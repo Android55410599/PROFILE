5a2ec67a0c5453d03d998a2ae059966d12d84197
test122333 <abc12345@gmail.com>
2014-09-10 14:45:31 +0700

    on

---
+++ Resume_55410559/src/com/example/resume_55410559/ImageActivity.java
@@ -0,0 +1,14 @@
+package com.example.resume_55410559;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImageActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.image_onwara);
+	}
+
+}
---
+++ SQLite_599/src/app/buusk15/sqlite_599/control599DB.java
@@ -0,0 +1,55 @@
+package app.buusk15.sqlite_599;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class control599DB extends SQLiteOpenHelper   { //SQLiteOpenHelper จัดการการสร้างและเปิด database
+
+	private static final String DATABASE_NAME = "mydata316";
+	private static final String TABLE_MEMBER = "members";
+	private static final int DATABASE_VERSION = 1;
+	
+	public control599DB(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION); //factory เป็นชื่อบริษัท
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub //คำสั่ง SQL CREATE TABLE  + ตามด้วยชื่อ  table  xxxx  + (ชื่อฟีว) 
+		db.execSQL("CREATE TABLE " + TABLE_MEMBER 
+				+ "(MemberID INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ "Name TEXT(100),"
+				+ "Tel TEXT(100));"); 
+		//CREATE TABLE member(MemberID INTEGER, Name TEXT(100),Tel TEXT());
+		
+		Log.d("CREATE TABLE", "Create Table Successfully"); 
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_MEMBER);
+		onCreate(db);
+	}
+	
+	public long InsertData(String strName, String strTel) {
+		try {
+			SQLiteDatabase db;
+			db = this.getWritableDatabase();
+			ContentValues values = new ContentValues();
+			values.put("Name", strName);
+			values.put("Tel", strTel);
+			long l = db.insert(TABLE_MEMBER, null, values);
+			db.close();
+			return l;
+		} catch (Exception e) {
+			return -1;   // try catch เอาไว้จับ error debug
+		}
+	}
+
+}
